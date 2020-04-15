7e085c60cdeb463da37eaf3124b26bd0c944caf7
unknown <Xivig>
2020-04-15 21:42:15 +0530

    Git bach command for commit

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -24,3 +24,44 @@ $(function () {
      console.log(city.display(1955));
 
 });
+
+// Ajax for pure JavaScript
+//1. Create an ajaxRequest object:
+var ajaxRequest = new XMLHttpRequest();
+//Here the ajaxRequest variable name can be anything you want (may be ajax)
+
+// 2. Create a callback function
+ajaxRequest.onreadystatechange = function () {
+    if (ajaxRequest.readyState === 4) {
+        document.getElementById('ajax').innerHTML = ajaxRequest.responseText;
+    }
+};
+
+// 3. Open a Request :
+ajaxRequest.open('GET', 'sidebar.html');
+
+// 4. Send a request:
+ajaxRequest.send();
+
+
+// explain???
+(function () {
+    const nameList = {
+        names: [],
+        newName: null,
+        init: function (callback) {
+            this.newName = callback;
+        },
+        addName: function (name) {
+            this.names.push(name);
+            this.newName(name, this.names);
+        }
+    };
+    nameList.init(function (newName, allNames) {
+        console.log(newName);
+        console.log('The list of all Name: ', allNames);
+    });
+
+    nameList.addName('Cool');
+    nameList.addName('Robert');
+})();
